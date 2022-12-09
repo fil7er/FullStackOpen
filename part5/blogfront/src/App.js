d08c09a73b8f7e1ecbe700d06faddfaac6598d77
Filipe Souza <fil7er@users.noreply.github.com>
2022-12-09 22:53:24 +0000

    first css

--- part5/blogfront/src/App.js
+++ part5/blogfront/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import Menu from './components/nav/Menu'
+import './css/App.css'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -13,7 +14,7 @@ const App = () => {
   }, [])
 
   return (
-    <div>
+    <div class="app">
       <Menu />
       <h2>blogs</h2>
       {blogs.map(blog =>
